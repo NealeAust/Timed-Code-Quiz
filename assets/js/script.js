14c72d66cc8a550120b8f076b66a2937356e94fe
NealeAust <nealechapman@optushome.com.au>
2022-12-17 13:01:45 +1100

    updated commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,41 +5,58 @@ btn.addEventListener("click", function () {
     var question = document.querySelector("#question")
     question.style.display = "block"
     Questions()
+    startCountdown()
 })
 
+
+var index = 0
+
 function Questions() {
-for (let i = 0; i < allQuestions.length; i++) {
-    console.log(allQuestions[0])
-    console.log(allQuestions[0].question)
+
+    console.log(allQuestions[index])
+    console.log(allQuestions[index].question)
     var questionel = document.querySelector("h3")
-    questionel.textContent = allQuestions[0].question
-    console.log(allQuestions[0].choices[0])
+    questionel.textContent = allQuestions[index].question
+    console.log(allQuestions[index].choices[0])
     var button1 = document.getElementById("choice1")
-    button1.textContent = allQuestions[0].choices[0]
+    button1.textContent = allQuestions[index].choices[0]
     button1.addEventListener("click", function () {
         //check if correct answer 
-        
+       index++ 
+       Questions()
     })
     var button2 = document.getElementById("choice2")
-    button2.textContent = allQuestions[0].choices[1]
+    button2.textContent = allQuestions[index].choices[1]
     button2.addEventListener("click", function () {
-        //check if correct answer 
+    //     if(choices2 = allQuestions[index].correctAnswer){
+            // Increase score++
+
+    //     } else {
+        //    Deduct 15 seconds off clock   
+
+    //  }
+
         
+        index++ 
+       Questions()
     })
     var button3 = document.getElementById("choice3")
-    button3.textContent = allQuestions[0].choices[2]
+    button3.textContent = allQuestions[index].choices[2]
     button3.addEventListener("click", function () {
         //check if correct answer 
-        
+        index++
+        Questions()
     })
     var button4 = document.getElementById("choice4")
-    button4.textContent = allQuestions[0].choices[3]
+    button4.textContent = allQuestions[index].choices[3]
     button4.addEventListener("click", function () {
         //check if correct answer 
-      
+     index++ 
+     Questions()
     })
 }
-}
+
+
 var allQuestions = [{
     question: 'Commonly used data types DO NOT include:',
     choices: ['1. strings', '2. booleans', '3. alerts', '4. numbers'],
@@ -56,12 +73,12 @@ var allQuestions = [{
     correctAnswer: 4
 },
 {
-    question: 'String values must be enclosed with _______ when being assigned to variables',
+    question: 'String values must be enclosed with _______  when being assigned to variables',
     choices: ['1. commas', '2. curly brackets', '3. quotes', '4. parenthesis'],
     correctAnswer: 3
 },
 {
-    question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    question: 'A very useful tool used during development and\ndebugging for printing content to the debugger is:',
     choices: ['1. JavaScript', '2. terminal/bash', '3. for loops', '4. console.log'],
     correctAnswer: 4
 }];
